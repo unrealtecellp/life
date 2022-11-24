19faa5031c39c6ddbcf7039b92feae540bd3616b
siddharth <drudgery.profession@gmail.com>
2022-11-24 22:43:04 +0530

    fetch access code list for karya extionsion

---
+++ app/karya_ext/static/fetchaccesscodelist.js
@@ -0,0 +1,54 @@
+function createSelectElement(key, elevalue, type, quesdatavalue) {
+    // console.log(quesdatavalue)
+    var qform = '';
+    var keyid = key.replace(new RegExp(' ', 'g'), '_');
+    // qform += '<div class="form-group">'+
+    qform += '<label for="'+keyid+'">'+key+': </label>';
+
+    if (type === 'multiple') {
+      qform += '<select class="fetchaccodeselect" id="'+keyid+'" name="'+key+'" multiple="'+type+'" style="width: 37%" required>';
+    }
+    else {
+      qform += '<select class="fetchaccodeselect" id="'+keyid+'" name="'+key+'" style="width: 37%" required>';
+    }
+    
+    for (let i=0; i<elevalue.length; i++) {
+      eval = elevalue[i]
+      // console.log(eval, quesdatavalue, quesdatavalue.includes(eval))
+      if (type === 'multiple') {
+        if (quesdatavalue.includes(eval)) {
+          qform += '<option value="'+eval+'" selected>'+eval+'</option>';  
+        }
+        else {
+          qform += '<option value="'+eval+'">'+eval+'</option>';
+        }
+      }
+      else {
+        if (quesdatavalue.includes(eval)) {
+          // console.log(eval, quesdatavalue)
+          qform += '<option value="'+eval+'" selected>'+eval+'</option>';  
+        }
+        else {
+          qform += '<option value="'+eval+'">'+eval+'</option>';
+        }
+      }
+    }
+    qform += '</select>';
+    // qform += '</select></div>';
+  
+    return qform;
+  }
+
+function fetchAccessCodeList(fetchaccesscodelist) {
+    console.log(fetchaccesscodelist);
+    var fetchaccodeform = ''
+    fetchaccodeform += createSelectElement('Access Code', fetchaccesscodelist, '', [])
+
+    $('#piaccesscode').html(fetchaccodeform);
+    $('.fetchaccodeselect').select2({
+        placeholder: 'select',
+        // data: usersList,
+        allowClear: true
+    });
+}
+  
