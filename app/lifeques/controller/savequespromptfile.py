3e5915a8cb47613a6c758f43ccfe0f8636fa269d
riteshkrjnu <riteshkrjnu@gmail.com>
2024-07-04 18:40:41 +0530

    Lang name with space bug fix

--- app/lifeques/controller/savequespromptfile.py
+++ app/lifeques/controller/savequespromptfile.py
@@ -9,16 +9,16 @@ logger = life_logging.get_logger()
 
 
 def savequespromptfile(mongo,
-                    projects,
-                    userprojects,
-                    projectsform,
-                    questionnaires,
-                    projectowner,
-                    activeprojectname,
-                    current_username,
-                    last_active_ques_id,
-                    new_file,
-                    **kwargs):
+                       projects,
+                       userprojects,
+                       projectsform,
+                       questionnaires,
+                       projectowner,
+                       activeprojectname,
+                       current_username,
+                       last_active_ques_id,
+                       new_file,
+                       **kwargs):
     """mapping of this function is with the 'uploadaudiofiles' route.
 
     Args:
@@ -34,7 +34,8 @@ def savequespromptfile(mongo,
     """
 
     # print("last_active_ques_id in savequesaudiofiles()", last_active_ques_id)
-    ques_form = projectsform.find_one({"projectname": activeprojectname}, {"_id": 0})
+    ques_form = projectsform.find_one(
+        {"projectname": activeprojectname}, {"_id": 0})
     # pprint(ques_form)
     new_file_details = {}
     for kwargs_key, kwargs_value in kwargs.items():
@@ -42,42 +43,44 @@ def savequespromptfile(mongo,
 
     prompt_type_info = list(new_file.keys())[0]
     prompt_type = prompt_type_info.split('_')[1]
+    # prompt_lang = prompt_type_info.split('_')[2:]
+    # prompt_lang = ' '.join(prompt_lang).strip()
     prompt_lang = prompt_type_info.split('_')[-1]
     # logger.debug('prompt_type_info: %s, prompt_type: %s, prompt_lang: %s',
     #              prompt_type_info, prompt_type, prompt_lang)
     if new_file[prompt_type_info].filename != '':
         filename = new_file[prompt_type_info].filename
         file_id = prompt_type[0]+re.sub(r'[-: \.]', '', str(datetime.now()))
-        updated_filename = (file_id+
-                                '_'+
-                                filename)
+        updated_filename = (file_id +
+                            '_' +
+                            filename)
 
     try:
-        
+
         # questionnaire_doc_id = questionnaires.update_one({'quesId': last_active_ques_id},
-        #                                                     {"$set": { 
+        #                                                     {"$set": {
         #                                                         "prompt.Transcription.audioFilename": updated_filename,
         #                                                         "prompt.Transcription.audioId": file_id,
         #                                                         "prompt.Transcription.audioLanguage": ques_form["Transcription"][1],
         #                                                         "prompt.otherInfo": new_file_details
         #                                                         }})
         questionnaire_doc_id = questionnaires.update_one({'quesId': last_active_ques_id},
-                                                            {"$set": { 
-                                                                "prompt.content."+prompt_lang+"."+prompt_type.lower()+".filename": updated_filename,
-                                                                "prompt.content."+prompt_lang+"."+prompt_type.lower()+".fileId": file_id,
-                                                                "prompt.content."+prompt_lang+"."+prompt_type.lower()+".otherInfo": new_file_details
-                                                                }})
+                                                         {"$set": {
+                                                             "prompt.content."+prompt_lang+"."+prompt_type.lower()+".filename": updated_filename,
+                                                             "prompt.content."+prompt_lang+"."+prompt_type.lower()+".fileId": file_id,
+                                                             "prompt.content."+prompt_lang+"."+prompt_type.lower()+".otherInfo": new_file_details
+                                                         }})
         # save audio file details in fs collection
         fs_file_id = mongo.save_file(updated_filename,
-                        new_file[prompt_type_info],
-                        fileId=file_id,
-                        username=projectowner,
-                        projectname=activeprojectname,
-                        updatedBy=current_username)
+                                     new_file[prompt_type_info],
+                                     fileId=file_id,
+                                     username=projectowner,
+                                     projectname=activeprojectname,
+                                     updatedBy=current_username)
 
         return (True, questionnaire_doc_id, fs_file_id)
 
     except Exception as e:
         print(e)
         flash(f"ERROR")
-        return(False,"","")
+        return (False, "", "")
